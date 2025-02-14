6153db833f53406a006ac1a2fa0f5eac5cb84bfd
Alexandra Kiseleva <sahsakis2512@gmail.com>
2025-02-14 19:20:18 +0400

    Added vote_average

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -11,6 +11,15 @@ const { Title } = Typography;
 const MovieCard = ({ item, genres = {} }) => {
   const [rating, setRating] = useState(0);
 
+  const ratingValue = Number(item?.vote_average).toFixed(1);
+
+  const ratingColor = (ratingValue) => {
+    if (ratingValue >= 7) return '#66E900';
+    if (ratingValue >= 5) return '#E9D100';
+    if (ratingValue >= 3) return '#E97E00';
+    return '#E90000';
+  };
+
   useEffect(() => {
     const savedRatings = JSON.parse(localStorage.getItem('rated_movies')) || {};
     if (savedRatings[item.id]) {
@@ -53,11 +62,32 @@ const MovieCard = ({ item, genres = {} }) => {
         preview={false}
       />
       <div style={{ flex: 1 }}>
-        <Title level={5}>{item?.title || item?.name}</Title>
+        <Title style={{ width: '84%' }} level={5}>
+          {item?.title || item?.name}
+        </Title>
         <MovieDate releaseDate={item?.release_date} firstAirDate={item?.first_air_date} />
         <MovieGenres genreIds={item?.genre_ids} genres={genres} />
         <MovieOverview overview={item?.overview} />
         <Rate value={rating} onChange={handleRateChange} count={10} />
+        <div
+          style={{
+            position: 'absolute',
+            top: '22px',
+            right: '25px',
+            fontSize: '12px',
+            width: '30px',
+            height: '30px',
+            borderRadius: '50%',
+            background: 'transparent',
+            border: '2px solid',
+            borderColor: ratingColor(ratingValue),
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {ratingValue}
+        </div>
       </div>
     </div>
   );
